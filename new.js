24aef62a0b275690c343d2719b05580fd58a9ba9
sabeekbinsayeed <sabikbinsayeed@gmail.com>
2022-02-17 20:55:22 +0600

    last

--- new.js
+++ new.js
@@ -1,24 +1,62 @@
+function fieldClear() {
+    document.getElementById('income-field').value = ''
+    document.getElementById('food-field').value = ''
+    document.getElementById('rent-field').value = ''
+    document.getElementById('clothes-field').value = ''
+    document.getElementById('save-field').value = ''
+    document.getElementById('total-expenses').innerText = '';
+    document.getElementById('balance').innerText = ''
+    document.getElementById('remaining-balance').innerText = '';
+    document.getElementById('saving-amount').innerText = ''
+}
+
 function getValue(inputField) {
     fieldValue = document.getElementById(inputField).value;
     if (fieldValue < 0) {
+        document.getElementById('error').style.display = 'block';
+        fieldClear();
+    }
+    if (isNaN(fieldValue)) {
         document.getElementById('error').style.display = 'block'
+        fieldClear();
+
     }
 
     return fieldValue;
 
 }
+
+function getExpense(food, rent, clothes) {
+    foodValue = parseFloat(food);
+    rentValue = parseFloat(rent);
+    clothesValue = parseFloat(clothes);
+    total = foodValue + rentValue + clothesValue;
+
+
+    return total;
+}
+
+
 document.getElementById('calculate').addEventListener('click', function () {
-    // const foodInput = document.getElementById('food-field');
+    document.getElementById('saving').style.display = 'none'
+    document.getElementById('expense').style.display = 'none'
+    document.getElementById('error').style.display = 'none'
+
+
+
     const incomeValue = getValue('income-field');
     const foodValue = getValue('food-field');
     const rentValue = getValue('rent-field');
     const clothesValue = getValue('clothes-field');
 
-    const total = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothesValue)
+
+    const total = getExpense(foodValue, rentValue, clothesValue);
     console.log(typeof total)
 
     if (total > parseFloat(incomeValue)) {
         document.getElementById('expense').style.display = 'block'
+        //field clear
+        fieldClear();
     }
 
 
@@ -31,6 +69,7 @@ document.getElementById('calculate').addEventListener('click', function () {
     }
     else {
         document.getElementById('error').style.display = 'block'
+
     }
 
 
@@ -50,6 +89,11 @@ document.getElementById('save-button').addEventListener('click', function () {
 
 
         document.getElementById('saving').style.display = 'block'
+        fieldClear();
+    }
+    else if (isNaN(savingTotal) || savingTotal < 0) {
+        document.getElementById('saving').style.display = 'block'
+        fieldClear();
     }
     else {
         document.getElementById('saving-amount').innerText = savingTotal;
